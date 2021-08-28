77f47e7ce910e6c7c975cc9b200f5ed661fa3fd6
gabrieleng <gabe4937@gmail.com>
2021-08-28 04:12:16 +0000

    adding select region function

---
+++ selectRegion.py
@@ -0,0 +1,7 @@
+def selectRegion(window)):
+    print("Please select region")
+    #pass window
+    #add callback
+    #remove callback
+    #cv2.setMouseCallback(window_name, lambda *args : None)
+    #return coords

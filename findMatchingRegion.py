aec1b715599f75e22f51720b2919832976cf9e4d
gabrieleng <gabe4937@gmail.com>
2021-08-28 21:38:35 -0700

    Added findMatchingRegion

---
+++ findMatchingRegion.py
@@ -0,0 +1,54 @@
+import numpy as np
+import cv2
+
+def findMatchingRegion(img, sourceCoords, searchCoords): #coords come like [x1,x2,y1,y2] assuming x2<x1,y2<y1
+    sourceWidth = sourceCoords[2]-sourceCoords[0]   
+    sourceHeight = sourceCoords[3]-sourceCoords[1]
+    #ceneter rectangles around eachother
+    xCentered = searchCoords[0] + round ( ( (searchCoords[2]-searchCoords[0]) - (sourceCoords[2]-sourceCoords[0]) ) / 2 )
+    yCentered = searchCoords[1] + round ( ( (searchCoords[3]-searchCoords[1]) - (sourceCoords[3]-sourceCoords[1]) ) / 2 )
+
+    #show source region
+    sourceRegion = img[sourceCoords[1]:sourceCoords[3],sourceCoords[0]:sourceCoords[2]].copy()
+    cv2.imshow("source", sourceRegion)
+    #slide window around this point
+    sumOfDiffs={}
+    for i in range(0,sourceWidth):
+        for j in range(0,sourceHeight):
+            x1=xCentered-round(sourceWidth/2)+i
+            y1=yCentered-round(sourceHeight/2)+j
+            x2=x1+sourceWidth
+            y2=y1+sourceHeight
+            #compare to source reagion
+            testRegion = img[y1:y2,x1:x2].copy()
+            cv2.imshow("scanning...", testRegion)
+            diff = cv2.absdiff(testRegion,sourceRegion)
+            cv2.imshow("comparing...", diff)
+            sumOfDiffs[str(i)+','+str(j)]=sum(sum(diff))
+            cv2.waitKey(1)
+
+    index=min(sumOfDiffs , key=sumOfDiffs.get)
+    print(sumOfDiffs)
+    print(index)
+    print(sumOfDiffs[index])
+
+    i = int(index.split(',')[0])
+    j = int(index.split(',')[1])
+    x1=xCentered-round(sourceWidth/2)+i
+    y1=yCentered-round(sourceHeight/2)+j
+    x2=x1+sourceWidth
+    y2=y1+sourceHeight
+    #compare to source reagion
+    testRegion = img[y1:y2,x1:x2].copy()
+    cv2.imshow("scanning...", testRegion)
+    diff = cv2.absdiff(testRegion,sourceRegion)
+    cv2.imshow("comparing...", diff)
+
+    cv2.waitKey(0)
+
+img = cv2.imread("img/JVASP-27851_Positive_20.jpg")
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(img, 100,200)
+laplacian = cv2.Laplacian(img,cv2.CV_64F)
+
+findMatchingRegion(laplacian, [193,74,217,109], [206,104,243,143])
--- selectRegion.py
+++ selectRegion.py
@@ -21,12 +21,19 @@ def selectRegion(window):
         cv2.waitKey(1)
     return coords
 
+def measureLength(window):
+    r = selectRegion('img')
+    print(r)
+    l = np.sqrt( (r[0]-r[2])**2 + (r[1]-r[3])**2 ) 
+    print(l)
 
 img = cv2.imread("img/JVASP-27851_Positive_20.jpg")
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imshow('img', img)
-r = selectRegion('img')
-print(r)
+
+measureLength('img')
+
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()

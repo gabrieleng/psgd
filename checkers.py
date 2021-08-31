1f1255fb49c0e6c56c467ff6dacffdc41b7f52aa
Gabriel Eng <gabe4937@gmail.com>
2021-08-30 19:32:28 -0700

    added checkers.py

---
+++ checkers.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+img = np.zeros((1024,1024), np.uint8)
+print(img.shape[0]/16)
+
+for i in range(0,img.shape[0]):
+    for j in range(0,img.shape[1]):
+        img[i,j] = int(  ( np.floor(i/64)+np.floor(j/64) )%2   * 255)
+cv2.imwrite("checkers.jpg", img)

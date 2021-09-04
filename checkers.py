4461a08bc5871cab44c26a76e2b8846cfb305a44
gabrieleng <gabe4937@gmail.com>
2021-09-03 21:01:58 -0700

    Cleaned up img folder

--- checkers.py
+++ checkers.py
@@ -7,4 +7,4 @@ print(img.shape[0]/16)
 for i in range(0,img.shape[0]):
     for j in range(0,img.shape[1]):
         img[i,j] = int(  ( np.floor(i/64)+np.floor(j/64) )%2   * 255)
-cv2.imwrite("checkers.jpg", img)
+cv2.imwrite("img/checkers.jpg", img)
